1a96def48849623a4525933795bd727bae20019c
Meer Estiyak <mmestiyak@gmail.com>
2023-06-20 20:40:01 +0600

    episode 3

---
+++ dom.js
@@ -0,0 +1,22 @@
+const heading = document.querySelector('h1') 
+/**
+here we are grabbing first h1 element in our document with the help of querySelector method and the method's argument is css selectors
+* */
+
+console.log(heading)
+
+
+const babyZone = document.querySelector('.baby-zone')
+const img = babyZone.querySelector('img') 
+/**  
+ not only in document we can query but also in elements to query it's inner elements
+* */
+
+console.log(img)
+
+
+// learn more about async defer attribute in script here
+// https://javascript.info/script-async-defer
+
+
+
